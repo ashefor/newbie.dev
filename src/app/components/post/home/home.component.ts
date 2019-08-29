ba38c4a9273d84c7e687c134046b362d888f59bc
ashefor <thatsureboy@gmail.com>
2019-08-29 16:40:39 +0100

    implement truncate text on home page

--- src/app/components/post/home/home.component.ts
+++ src/app/components/post/home/home.component.ts
@@ -1,32 +1,53 @@
 import { posts } from '../../../model/post.model';
 import { PostService } from '../../../services/home.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChildren, ElementRef, QueryList, ViewChild, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, AfterViewInit {
   allposts = [];
   posts: posts[]
   nodata;
   // wordsPerMinute =  200;
   result = [];
   showTags = false;
-  alltags = []
+  alltags = [];
+  @ViewChildren('postBody') postBodyy: QueryList<any>
+  // @ViewChild('postBody') postBodyy: ElementRef
   constructor(private hs: PostService) { }
 
   ngOnInit() {
-    this.hs.getAllPosts().subscribe((data:any) =>{
-     if(data){
-      console.log(data)
-      this.posts = data;
-      for(let obj of data){
-        this.readingTime(obj.body)
-        this.alltags.push(obj.meta.tags)
+    this.hs.getAllPosts().subscribe((data: any) => {
+      if (data) {
+        console.log(data)
+        this.posts = data;
+        for (let obj of data) {
+          this.readingTime(obj.body)
+          this.alltags.push(obj.meta.tags);
+        }
       }
-     }
+    })
+  }
+  ngAfterViewInit() {
+    setTimeout(() => {
+      this.viewBody('...')
+    }, 1000);
+  }
+  viewBody(after) {
+    this.postBodyy.forEach(body => {
+      const limit = 50;
+      let content = body.nativeElement.innerHTML.trim()
+      content = content.split(' ')
+      if (content.length <= limit) {
+        return content
+      } else {
+        content = content.slice(0, limit);
+        content = content.join(' ') + (after ? after : '');
+      }
+      body.nativeElement.innerHTML = content;
     })
   }
   readingTime(body) {
@@ -34,5 +55,16 @@ export class HomeComponent implements OnInit {
     const noOfWords = body.split(/\s/g).length;
     const minutes = noOfWords / wordsPerMinute;
     this.result.push(Math.ceil(minutes));
+  }
+  truncateText(body, after) {
+    const limit = 10;
+    let content = body.trim()
+    content = content.split(' ').slice(0, limit);
+
+    content = content.join(' ') + (after ? after : '');
+
+    body.textContent = content
+
+    console.log(body)
   }
 }
